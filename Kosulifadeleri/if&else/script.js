4675ddd3b7e537b29511f91fe1d510358a818e24
Hüseyin Dönmez <61841165+deavepup@users.noreply.github.com>
2021-03-10 19:49:34 +0300

    kosul ifadeleri

---
+++ Kosulifadeleri/if&else/script.js
@@ -0,0 +1,52 @@
+// if/else statements
+
+const firstName='Hüseyin';
+const age = 21;
+const isStudent = true;
+const school = 'university';
+
+ //if(firstName === 'Hüseyin'){
+  //   console.log('Merhaba Hüseyin');
+// }
+
+// if (age === 21){
+//     console.log('yaşınız 21');
+// }
+
+// if (isStudent){
+//     console.log('Merhaba öğrenci');
+// }else{
+//     console.log('Merhaba işler nasıl');
+// }
+
+
+// if (age>=18){
+
+//     if ((school == 'university') || (school == 'high school')){
+//         console.log('ehliyet alabilirsiniz');
+//     } else{
+//         console.log('eğitim durumunuz yetersiz');
+//     }
+
+// }else{
+//     console.log('ehliyet alamazsınız');
+// }
+
+
+// if (age > 0  && age<12 ){
+//     console.log(`${firstName} is a child`);
+// }else if (age >=13 && age <=19){
+//     console.log(`${firstName} is a teenager`);
+// }else{
+//     console.log(`${firstName} is an adult`);
+// }
+
+//undefined
+
+//let id =123;
+
+if (typeof id !== 'undefined'){
+    console.log('id: '+id);
+}else{
+    console.log('no id');
+}
